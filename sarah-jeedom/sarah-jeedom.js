1623498780ea7683a02c1b8fb320efcaf32e02ca
Steven <steven@pruvost.ninja>
2018-02-28 22:18:56 +0100

    Add scenario + corrections + refactoring
    Ajout de la prise en compte des scenario Jeedom
    Divers corrections pour un bon fonctionnement
    Refactoring + mise en page du code

--- sarah-jeedom/sarah-jeedom.js
+++ sarah-jeedom/sarah-jeedom.js
@@ -1,165 +1,145 @@
-
 var request = require('request');
 
 module.exports = function(RED) {
-    function sarahjeedom(config) {
-        RED.nodes.createNode(this,config);
 
-		this.host = config.host;
-		this.port = config.port;
-		this.apikey = config.apikey;
-		
+    function sarahjeedom(config) {
+
+        RED.nodes.createNode(this, config);
+        this.host = config.host;
+        this.port = config.port;
+        this.apikey = config.apikey;
         var node = this;
 
-		
-		
+        node.on('input', function(msg) {
 
-		node.on('input', function(msg) {
-			var url = "http://"+node.host+":"+node.port+"/jeedom/core/api/jeeApi.php?apikey="+node.apikey;
-			
-			//Récupération des paramètres provenant du fichier grammar/sarah-domoticz.xml retourné par win-sarah
-			var action = msg.payload.options.action;
-			var command = msg.payload.options.command;
-			var cmdid = msg.payload.options.cmdid;
-			var tts = msg.payload.options.tts;
+            var url = "http://" + node.host + ":" + node.port + "/core/api/jeeApi.php?apikey=" + node.apikey;
+            //Récupération des paramètres provenant du fichier grammar/sarah-domoticz.xml retourné par win-sarah
+            var action = msg.payload.options.action;
+            var device = msg.payload.options.device;
+            var type = msg.payload.options.type;
+            var cmdid = msg.payload.options.cmdid;
+            var tts = msg.payload.options.tts;
+            //teste si la variable commande est une action ou une demande de status
 
-			//teste si la variable commande est une action ou une demande de status
-			switch(action){
-				
-				case "On":
-					if(cmdid){
-						url = url+'&type=cmd&id='+cmdid;
-						msg.speak = "c'est fait"
-						
-					}
-					
-					else{
-						msg.speak = "Il semble que la configuration soit invalide";
-					}
-					
-					break;
+            switch (action) {
+            case "On":
+            case "Off":
+                if (cmdid) {
+                    url = url + '&type=cmd&id=' + cmdid;
+                    msg.speak = "c'est fait";
 
-				case "Off":
-					if(cmdid){
-						url = url+'&type=cmd&id='+cmdid;
-						msg.speak = "c'est fait"
-					}
-					
-					else{
-						msg.speak = "Il semble que la configuration soit invalide";
-					}
-					
-					break;
+                } else {
+                    msg.speak = "Il semble que la configuration soit invalide";
 
-				case "status":
-					if (cmdid){
-						url = url+'&type=cmd&id='+cmdid;
-					}
-					
-					else {
-					msg.speak = "Il semble que la configuration soit invalide";;
-					}
-				
-			}
+                }
+                break;
+            case "status":
+                if (cmdid) {
+                    url = url + '&type=cmd&id=' + cmdid;
+                } else {
+                    msg.speak = "Il semble que la configuration soit invalide";
 
-			//effectue la requete http à jeedom
-			request(url, function (error, response, body) {
-				msg.url = url
-				node.status({});
-				if (error) {
+                }
+                break;
+            case "start":
+                if (cmdid) {
+                    url = url + '&type=scenario&id=' + cmdid + '&action=' + action;
+                    msg.speak = "le scénario a été lancé";
 
-					if (error.code === 'ETIMEDOUT') {
+                } else {
+                    msg.speak = "Il semble que la configuration soit invalide";
 
-						setTimeout(function () {
-						node.status({
-							fill: "red",
-							shape: "ring",
-							text: "common.notification.errors.no-response"
-						});
-						}, 2);
-						
-						msg.speak = "jeedom est injoignable";
-					
-					} 
-							
-					else {
+                }
+                break;
+            case "stop":
+                if (cmdid) {
+                    url = url + '&type=scenario&id=' + cmdid + '&action=' + action;
+                    msg.speak = "le scénario a été arrété";
 
-						node.error(error, msg);
-						msg.payload = error.toString() + " : " + url;
-						msg.statusCode = error.code;
-						//node.send(msg);
-						node.status({
-						fill: "red",
-						shape: "ring",
-						text: error.code
-						});
-						
-						msg.speak = "jeedom erreur "+error.code;
-							
-					} 
-						
-				}
-				
-				else {
-					
-					msg.resultat = body
-					if (action == "status"){
-						
-						var body = JSON.parse(body);
-						
-						msg.resultat = body
-						
-						switch (type){
-							
-							case "switch":
-								status = body.result[0].Status
-								
-								if (status == "On"){
-									
-									status = "allumée"
-								}
-								if (status == "Off"){
-									status = "éteinte"
-								}
-								
-								msg.speak = 'la lumière est '+status;
-							break;
-							
-							case "temp":
-								
-								status = body.result[0]
-								status = status.replace('.', ',')
-																
-								msg.speak = 'La température '+tts+' est de '+status+' degré';
-						
-							break;
-							
-							case "humidity":
-								
-								status = body.result[0]
-								status = status.replace('.', ',')
-																
-								msg.speak = 'L\'humidité '+tts+' est de '+status+' %';  
-						
-							break;
-							
-		
-						}
-						
-					}
-				
-				}
-	
-				node.send(msg);
+                } else {
+                    msg.speak = "Il semble que la configuration soit invalide";
 
-			});
-			
+                }
+                break;
+            case "activer":
+            case "désactiver":
+                if (cmdid) {
+                    url = url + '&type=scenario&id=' + cmdid + '&action=' + action;
+                    msg.speak = "le scénario a été " + action;
+
+                } else {
+                    msg.speak = "Il semble que la configuration soit invalide";
+
+                }
+                break;
+            default:
+                msg.speak = "Action invalide";
+            }
+
+            //effectue la requete http à jeedom
+            request(url, function(error, response, body) {
+
+                msg.url = url;
+                node.status({});
+
+                if (error) {
+                    if (error.code === 'ETIMEDOUT') {
+                        setTimeout(function() {
+                            node.status({
+                                fill : "red",
+                                shape : "ring",
+                                text : "common.notification.errors.no-response"
+                            });
+                        }, 2);
+                        msg.speak = "jeedom est injoignable";
+
+                    } else {
+                        node.error(error, msg);
+                        msg.payload = error.toString() + " : " + url;
+                        msg.statusCode = error.code;
+                        //node.send(msg);
+                        node.status({
+                            fill : "red",
+                            shape : "ring",
+                            text : error.code
+                        });
+                        msg.speak = "jeedom erreur " + error.code;
+
+                    }
+                } else {
+                    msg.resultat = body;
+                    
+                    if (action == "status") {
+                        switch (type) {
+                        case "switch":
+                            status = body;
+                            if (status == "On") {
+                                status = "allumée";
+                            }
+                            if (status == "Off") {
+                                status = "éteinte";
+                            }
+                            msg.speak = 'la lumière est ' + status;
+                            break;
+                        case "temp":
+                            var temp = body.replace('.', ',');
+                            msg.speak = 'La température ' + tts + ' est de '+ temp +' degré';
+                            break;
+                        case "humidity":
+                            var humidity = body.replace('.', ',');
+                            msg.speak = 'L\'humidité ' + tts + ' est de ' + humidity + ' %';
+                            break;
+                        default:
+                            msg.speak = "Type invalide";
+
+                        }
+                    }
+                }
+                node.send(msg);
+            });
         });
     }
-	
 
-    RED.nodes.registerType("sarah-jeedom",sarahjeedom);
+
+    RED.nodes.registerType("sarah-jeedom", sarahjeedom);
 }
-
-
-
